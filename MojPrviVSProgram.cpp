b428654fcce3240aca9f13b9ec590a829ce51ff7
kuki1234 <lukakuki6@gmail.com>
2023-05-21 21:57:32 +0200

    Initial commit

---
+++ MojPrviVSProgram.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	string* ucenici = new string[n];
+	cin.ignore();
+	for (int i = 0; i < n; i++)
+	{
+		getline(cin, ucenici[i]);
+	}
+	sort(ucenici, ucenici + n);
+	fstream datoteka;
+	datoteka.open("imenik.txt", ios::app);
+	for (int i = 0; i < n; i++)
+		datoteka << ucenici[i] << endl;
+	datoteka.close();
+	return 0;
+}
